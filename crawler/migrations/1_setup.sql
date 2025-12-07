8b363fe8cd277915852787913a01c1d611d9f84b
403F <4o3f@proton.me>
2025-12-07 21:28:23 +0800

    feat: add database initialization

---
+++ crawler/migrations/1_setup.sql
@@ -0,0 +1,27 @@
+CREATE TABLE anime (
+    id INT PRIMARY KEY,
+    platform TEXT,
+    image_url TEXT,
+    summary TEXT,
+    name TEXT,
+    name_cn TEXT,
+    tags JSONB,
+    infobox JSONB,
+    rating FLOAT,
+    rank INT,
+    air_date DATE,
+    episodes INT,
+    created_at TIMESTAMP DEFAULT NOW (),
+    updated_at TIMESTAMP DEFAULT NOW ()
+);
+
+CREATE TABLE users (id INT PRIMARY KEY, username TEXT, reg_date DATE);
+
+CREATE TABLE interactions (
+    user_id INT,
+    anime_id INT,
+    status INT,
+    rate INT,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (user_id, anime_id)
+);
--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,5 +1,11 @@
+#![deny(clippy::unwrap_used)]
+
+use std::env;
+
+use sqlx::postgres::PgPoolOptions;
 use tracing::{Level, error};
 use tracing_subscriber::FmtSubscriber;
+use tracing_unwrap::ResultExt;
 
 #[tokio::main]
 async fn main() {
@@ -14,5 +20,16 @@ async fn main() {
         return;
     }
 
-    
+    let db_connect_url =
+        env::var("DATABASE_URL").expect_or_log("Failed to get environment variable DATABASE_URL");
+    let db = PgPoolOptions::new()
+        .max_connections(50)
+        .connect(&db_connect_url)
+        .await
+        .expect_or_log("Could not connect to database");
+
+    sqlx::migrate!()
+        .run(&db)
+        .await
+        .expect_or_log("Failed to run database migration");
 }
